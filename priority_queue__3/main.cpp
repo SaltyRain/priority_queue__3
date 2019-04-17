9301a5dcb5c2d320f2eee8461142255f47f0c62d
Timur Garipov <tbgaripov@gmail.com>
2019-04-17 16:15:45 +0300

    Initial Commit

---
+++ priority_queue__3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  priority_queue__3
+//
+//  Created by Тимур Гарипов on 17/04/2019.
+//  Copyright © 2019 Timur. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
